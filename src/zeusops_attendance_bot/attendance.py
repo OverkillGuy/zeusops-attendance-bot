ff13665b1f925758f31a95801f9e7a350f815f56
Jb DOYON <jb@jiby.tech>
2022-11-17 01:12:01 +0000

    Add separate regex-based parsing

--- src/zeusops_attendance_bot/attendance.py
+++ src/zeusops_attendance_bot/attendance.py
@@ -1,4 +1,4 @@
-"""Parse the attendance messages as string"""
+"""Clean the attendance messages for regex processing"""
 
 import json
 from dataclasses import dataclass
@@ -36,20 +36,26 @@ def newline_separate(messages: LoadedAttendanceHistory) -> LoadedAttendanceHisto
     """Split messages containing newlines into new messages"""
     messages_out = []
     for msg in messages:
-        messages_out.append(msg)
         if "\n" not in msg.message:
+            messages_out.append(msg)
             continue
+        # Newline caught: split the message
         before_newline, *after_newline = msg.message.splitlines()
+        messages_out.append(
+            AttendanceMsg(
+                message=before_newline, author=msg.author, timestamp=msg.timestamp
+            )
+        )
         after_newline = [
             line for line in after_newline if line
         ]  # Skip evaluating repeated \n
         if len(after_newline) > 1:
-            print(
-                f"This message is weird, multiple separate newlines: '{msg.message=}'"
-            )
+            # print(
+            #     f"This message is weird, multiple separate newlines: '{msg.message=}'"
+            # )
             continue
         new_message = after_newline[0]
-        print(f"Split message: '{new_message}'")
+        # print(f"Split message: '{new_message}'")
         messages_out.append(
             AttendanceMsg(
                 message=new_message, author=msg.author, timestamp=msg.timestamp
@@ -58,13 +64,20 @@ def newline_separate(messages: LoadedAttendanceHistory) -> LoadedAttendanceHisto
     return messages_out
 
 
-def clean_formatting(msg: str) -> str:
+def clean_bold(msg: AttendanceMsg) -> AttendanceMsg:
     """Remove decorative markdown from given message"""
-    return msg.strip().replace("**", "").strip()
+    return AttendanceMsg(
+        message=msg.message.strip().replace("**", "").strip(),
+        author=msg.author,
+        timestamp=msg.timestamp,
+    )
 
 
 def main():
     """Parse entrypoint"""
     h = load_attendance()
     h2 = newline_separate(h)
+    h3 = [clean_bold(message) for message in h2]
     print(f"Before: {len(h)}, After: {len(h2)}")
+    with open("attendance.txt", "w") as attendance_fd:
+        attendance_fd.write("\n".join([msg.message for msg in h3]))
---
+++ src/zeusops_attendance_bot/parsing.py
@@ -0,0 +1,42 @@
+"""Parse attendance via regexes"""
+
+import re
+from pathlib import Path
+
+REGEX_OP_SEPARATOR = re.compile(r"""(---+|===+)""")
+"""Match an operation's separator"""
+
+REGEX_SQUAD = re.compile(
+    r"""
+   ([A-Za-z0-9]+)         # A squad name (TODO check for SPACES?)
+   \s*[:-;]\s*         # Separator between squad: team
+   ([a-zA-Z0-9\(\),;\. ]+)  # Attendance for squad, unparsed
+""",
+    re.VERBOSE,
+)
+"""Match a single line of squad attendance"""
+
+
+def main():
+    """Parse the cleaned up attendance text"""
+    attendance_txt = Path("attendance.txt").read_text()
+    op_separator_locations = [
+        (match.start(), match.end())
+        for match in re.finditer(REGEX_OP_SEPARATOR, attendance_txt)
+    ]
+    in_between_locations = [
+        (end0, start1)
+        for ((start0, end0), (start1, end1)) in zip(
+            op_separator_locations[:-1], op_separator_locations[1:]
+        )
+    ]
+    for start, end in in_between_locations:
+        start_offset, end_offset = start + 1, end - 1
+        op_attendance_text = attendance_txt[
+            start_offset, end_offset
+        ]  # FIXME: Offset for \n
+        # print(f"Attendance for op is:\n{op_attendance_text}")
+        squad_matches = re.findall(REGEX_SQUAD, op_attendance_text)
+        for squad, attendance_of_squad in squad_matches:
+            print(f"For {squad=}, attendance: '{attendance_of_squad}'")
+            # TODO: Process the squad's attendance as regex
